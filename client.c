2b237e6e696ebdd7ca46448c690ba67e7d014a3a
zaotron <zao.naegele@gmail.com>
2019-12-04 22:47:54 -0800

    Initial commit

--- client.c
+++ client.c
@@ -8,6 +8,7 @@
 
 static const int kStrandCnt = 3;
 static const int kLEDCnt = 120;
+static const int newstrandcount =8;
 
 struct strand {
   int sock;
@@ -18,35 +19,59 @@ void effect(struct strand *s, int w, int h) {
   char matrix[kStrandCnt][kLEDCnt * 3];
   int pixel[kStrandCnt];
 
+
+//This loop below does the initial color of the full LED strands before the flash
   for (int i = 0; i < kStrandCnt; ++i)
     for (int j = 0; j < kLEDCnt; ++j) {
-      matrix[i][j *3 + 0] = 0x10;
-      matrix[i][j *3 + 1] = 0x0;
-      matrix[i][j *3 + 2] = 0x0;
+      //for (int d = 0x00; d < 0xFF; d++) {
+        matrix[i][j *3 + 0] = 0x00; //red
+        matrix[i][j *3 + 1] = (j * kLEDCnt)/255; //green
+        matrix[i][j *3 + 2] = 0x00; //blue
+        //usleep(1000);
+      //}
     }
 
-  for (int c = 0; c < 500; ++c) {
+// The loop below dictates brightness values for RGB for each sparkle pixel
+  for (int c = 0; c < 500; ++c) {  // c represents number of times to loop through sparkle pixel
     for (int i = 0; i < kStrandCnt; ++i) {
       pixel[i] = rand() % kLEDCnt;
-      matrix[i][pixel[i] * 3 + 0] = 0xff;
-      matrix[i][pixel[i] * 3 + 1] = 0x00;
-      matrix[i][pixel[i] * 3 + 2] = 0x00;
-      if (send(s[i].sock, matrix[i], kLEDCnt*3, 0) < 0) {
-        fprintf(stderr, "Send failed");
-        return;
+        for (int b = 0; b < 256; b++) { //newly added loop to increase color slowly
+          matrix[i][pixel[i] * 3 + 0] = b; //red
+          matrix[i][pixel[i] * 3 + 1] = 0x00; //green
+          matrix[i][pixel[i] * 3 + 2] = 0x00; //blue
+          if (send(s[i].sock, matrix[i], kLEDCnt*3, 0) < 0) {
+            fprintf(stderr, "Send failed");
+            return;
+          }
+          usleep(100);
       }
-    }
 
-    usleep(2000);
+      for (int b = 255; b >= 0; b--) { //newly added loop to decrease color slowly
+          matrix[i][pixel[i] * 3 + 0] = b; //red
+          matrix[i][pixel[i] * 3 + 1] = 0x00; //green
+          matrix[i][pixel[i] * 3 + 2] = 0x00; //blue
+          if (send(s[i].sock, matrix[i], kLEDCnt*3, 0) < 0) {
+            fprintf(stderr, "Send failed");
+            return;
+          }
+          usleep(100);
+      }
+    }
+     fprintf(stdout, "Loop 1");
+
+    usleep(60000);
+    // This loop here will dictate what the color of the pixel led will end on the blink
+    /*
     for (int i = 0; i < kStrandCnt; ++i) {
-      matrix[i][pixel[i] * 3 + 0] = 0x10;
-      matrix[i][pixel[i] * 3 + 1] = 0x00;
-      matrix[i][pixel[i] * 3 + 2] = 0x00;
+      matrix[i][pixel[i] * 3 + 0] = 0x00; //red
+      matrix[i][pixel[i] * 3 + 1] = 0x00; //green
+      matrix[i][pixel[i] * 3 + 2] = 0x00; //blue
       if (send(s[i].sock, matrix[i], kLEDCnt*3, 0) < 0) {
         fprintf(stderr, "Send failed");
         return;
       }
     }
+    */
     usleep(10000 + rand() % 90000);
   }
 }
@@ -86,6 +111,14 @@ int main(int c, char **v) {
   strands[0].host = 209;
   strands[1].host = 205;
   strands[2].host = 218;
+  /*strands[3].host = 201;
+  strands[4].host = 202;
+  strands[5].host = 203;
+  strands[6].host = 204;
+  strands[7].host = 206;
+  strands[8].host = 207;
+  */
+  
 
   for (int i = 0; i < kStrandCnt; ++i)
     createConnection(&strands[i]);
